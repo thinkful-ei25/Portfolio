d227452b89cb4b514edd4b21ce4f79b7ef8cac8a
Sean Nealon <signalflowsean@gmail.com>
2018-12-12 09:27:31 -0500

    added nav button

--- src/Components/project.js
+++ src/Components/project.js
@@ -10,6 +10,19 @@ export default class Project extends React.Component{
                 <YouTube className="youTube" 
                     videoId="rr6f26I2cBI"          
                 ></YouTube>
+                <input 
+                    type="image" 
+                    src="https://i.guim.co.uk/img/media/a6b497e9ff1b9f296ee879d3ec7d175cda4f5866/177_762_3472_4340/master/3472.jpg?width=300&quality=85&auto=format&fit=max&s=29fae83eea77fc46008cb26ee15bbcd6" 
+                    alt="left arrorw" 
+                    className="left projectNav"
+                    onClick={() => console.log('cheese')}>
+                </input>
+                <input 
+                    type="image" 
+                    src="https://i.guim.co.uk/img/media/a6b497e9ff1b9f296ee879d3ec7d175cda4f5866/177_762_3472_4340/master/3472.jpg?width=300&quality=85&auto=format&fit=max&s=29fae83eea77fc46008cb26ee15bbcd6"
+                    className="right projectNav"
+                    onClick={() => console.log('cheese')}>
+                </input>
             </section>
         ); 
     }
