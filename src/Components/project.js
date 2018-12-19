d2f3eb5225f2e1999958ba2d6047781d0cd6182c
Sean Nealon <signalflowsean@gmail.com>
2018-12-19 13:41:04 -0500

    changing around stylez

--- src/Components/project.js
+++ src/Components/project.js
@@ -36,7 +36,7 @@ export default class Project extends React.Component{
         this.player = player;
       };
      
-      onLeftButtonClick = (event) => {
+      onLeftButtonClick = () => {
         this.setState({
           id : videoIdRotateLeft(), 
           projectTitle : videoTitleRotateLeft(), 
@@ -44,12 +44,20 @@ export default class Project extends React.Component{
             () => this.player.loadVideoById(this.state.id)); 
       };
 
-      onRightButtonClick = (event) => { 
+      onRightButtonClick = () => { 
         this.setState({
           id: videoIdRotateRight(), 
           projectTitle: videoTitleRotateRight(),
           projectDescription : videoDescriptionRotateRight()},  
             () => this.player.loadVideoById(this.state.id)); 
+      }
+
+      onAboutMeButtonClick = () => { 
+        console.log('ABOUT ME')
+      }
+
+      onContactMeButtonClick = () => { 
+        console.log('CONTACT ME')
       }
      
     render() {
@@ -62,21 +70,36 @@ export default class Project extends React.Component{
               onReady={this.onReady}
               onPlayer={this.onPlayer}
             />
-           <input 
+           {/* <input 
               type="image" 
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/TriangleArrow-Left.svg/2000px-TriangleArrow-Left.svg.png" 
               alt="left arrorw" 
-              className="left projectNav"
+              className="left button projectNav"
               onClick={this.onLeftButtonClick}>
             </input>
             <input 
               type="image" 
               alt="right arrow"
               src="http://www.launchd.info/img/NSRightFacingTriangleTemplate.png"
-              className="right projectNav"
+              className="right button projectNav"
               onClick={this.onRightButtonClick}>
             </input>
-            <p><b>{this.state.projectTitle}: </b>{this.state.projectDescription}</p>
+            <input 
+              type="image"
+              src="https://cdn1.iconfinder.com/data/icons/education-set-4/512/information-512.png"
+              className="top button"
+              onClick={this.onAboutMeButtonClick}
+              alt="About Me"
+            ></input>
+            <input
+              type="image"
+              src="https://cdn3.iconfinder.com/data/icons/network-communication-vol-2-2/48/96-512.png"
+              className="bottom button"
+              onClick={this.onContactMeButtonClick}
+              alt="Contact Me"
+            >
+            </input>
+            <p><b>{this.state.projectTitle}: </b>{this.state.projectDescription}</p> */}
           </section>
         );
     }
